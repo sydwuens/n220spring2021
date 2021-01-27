8bb8f03a17505d92e3b89511daea938633558559
sydwuens <sydwuens@iu.edu>
2021-01-27 18:04:14 -0500

    Lab1 personal comp

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(800,600);
+
+    background(237, 210, 135)
+    stroke("red")
+    circle(100,100,30);
+    rect(400, 200, 30, 30);
+
+    stroke(2);
+    line(0,500,200,20);
+
+}
