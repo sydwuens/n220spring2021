5254ad53ad5f70b063d941a6c05c1a83898db219
sydwuens <sydwuens@iu.edu>
2021-02-10 16:54:33 -0500

    lab 4

--- labs/lab 4/js/dripping water/drippingwater.js
+++ labs/lab 4/js/dripping water/drippingwater.js
@@ -4,7 +4,7 @@ let y = [];
 function setup() {
   createCanvas(400, 300);
 	
-	for (var i = 0; i < 20; i = i + 1) {
+	for (var i = 0; i < 1; i++) {
     x[i] = 200;
     y[i] = 0;
   }
@@ -13,13 +13,10 @@ function setup() {
 function draw() {
   background(50);
   
-  // draw drip
-  // down 3 pixels each frame, but maybe should be accelerating?
-	for (var i = 0; i < 100; i = i + 2) {
+	for (var i = 0; i < 100; i++) {
     circle(x[i], y[i], 20);
-		y[i] = y[i] + random(0, 25);
-		if (y[i] > height*2) {
-			// reset
+		y[i] = y[i] + random(0, 5);
+		if (y[i] > height) {
 			y[i] = 0;
 		}
   }
