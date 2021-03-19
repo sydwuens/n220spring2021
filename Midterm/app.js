3089a500abe3c21cf2ddef514a55a2d4fef3d4e0
sydwuens <sydwuens@iu.edu>
2021-03-19 12:31:19 -0400

    midterm

--- Midterm/app.js
+++ Midterm/app.js
@@ -1,28 +1,28 @@
 // draw 10 circles 
 var circles = [
-    { x: 130, y: 20, r: 10},
-    { x: 100, y: 20, r: 10},
-    { x: 100, y: 20, r: 10},
-    { x: 100, y: 20, r: 10},
-    { x: 100, y: 20, r: 10},
-    { x: 100, y: 20, r: 10},
-    { x: 100, y: 20, r: 10},
-    { x: 100, y: 20, r: 10},
-    { x: 100, y: 20, r: 10},
-    { x: 100, y: 20, r: 10},
-  
-  ];
-  
-  // create canvas
-  function setup(){
-    createCanvas(400,300);
+  { x: 130, y: 20, r: 10},
+  { x: 120, y: 50, r: 10},
+  { x: 10, y: 20, r: 10},
+  { x: 150, y: 20, r: 10},
+  { x: 100, y: 20, r: 10},
+  { x: 100, y: 20, r: 10},
+  { x: 100, y: 20, r: 10},
+  { x: 100, y: 20, r: 10},
+  { x: 100, y: 20, r: 10},
+  { x: 100, y: 20, r: 10},
+
+];
+
+// create canvas
+function setup(){
+  createCanvas(400,300);
+}
+
+// animate circles down
+function draw(){
+  for( var i = 0; i < circles.length; i++){
+    let currentCircle = circles[i]
+    circle(circles[i].x, circles[i].y, circles[i].r);
+    currentCircle.y += 2;
   }
-  
-  // animate circles down
-  function draw(){
-    for( var i = 0; i < circles.length; i++){
-      let currentCircle = circles[i]
-      circle(circles[i].x, circles[i].y, circles[i].r);
-      currentCircle.y += 2;
-    }
-  }
+}
