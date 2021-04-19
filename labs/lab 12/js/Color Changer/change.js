b0c61bbf8d29621dfa926330bd0f95de3b070dca
sydwuens <sydwuens@iu.edu>
2021-04-19 12:44:31 -0400

    .....

--- labs/lab 12/js/Color Changer/change.js
+++ labs/lab 12/js/Color Changer/change.js
@@ -1,45 +1,18 @@
 //links index and js
-let mySquare = document.getElementById ("mySquare");
+let myChangeOne = document.getElementById ("dvSquareOne");
 
-//make 3 squares
-    let newEn = document.createElement ("div");
+let myChangeTwo = document.getElementById ("dvSquareTwo");
 
-    //styles squares
-    newEn.style.width = "200px";
-    newEn.style.height = "200px";
-    newEn.style.backgroundColor = "#70706e";
-    newEn.style.margin = "5px";
-    newEn.style.float = "left";
+let myChangeThree = document.getElementById ("dvSquareThree");
 
-    //makes squares visible on page
-    mySquare.appendChild(newEn);
+colors = ["#ff0000", "#15ff00", "#0800ff"]
 
-    newEn.addEventListener("div", changeColor);
+myChangeOne.addEventListener("click", sqClick);
+myChangeTwo.addEventListener("click", sqClick);
+myChangeThree.addEventListener("click", sqClick);
 
+function sqClick(event){
+  let colors = event.target.getAttribute("data-color");
 
-function changeColor(event){
-  event.target.style.backgroundColor = "#994";
-  
-  }
-
-  let newEl = document.createElement ("hi");
-
-    //styles squares
-    newEl.style.width = "20px";
-    newEl.style.height = "20px";
-    newEl.style.backgroundColor = "#70706e";
-    newEl.style.margin = "5px";
-    newEl.style.float = "left";
-
-    //makes squares visible on page
-    mySquare.appendChild(newEn);
-
-    newEl.addEventListener("hi", changeColorTwo);
-
-
-function changeColorTwo(event){
-  event.target.style.backgroundColor = "#ff0000";
-  
-  }
-
-  //#ff0000 #15ff00 #0800ff
+  event.target.style.backgroundColor = colors;
+}
--- labs/lab 12/js/Color Mixer/mix.js
+++ labs/lab 12/js/Color Mixer/mix.js
@@ -1,16 +1,49 @@
-//link js to html 
+//link to html
+canvas = document.getElementById("dvCanvas");
+currentColor = document.getElementById("dvCurrentColor");
 
-//set size, position and starting color of color changing rectangle
+//color variables
+let red = 0;
+let green = 0;
+let blue = 0;
 
-//buttons for red
+// increase the colors
 
-//function for onclick of all buttons
+function addColor(event){
+    var getColor = event.target.getAttribute("data-color");
+    var getValue = event.target.getAttribute("data-value");
 
-//buttons for green
+    if(getColor == "red"){
+        if(getValue == 1){
+            red++;
+        } else if(getValue == 5){
+            red+= 5;
+        } else if(getValue == 10){
+            red+= 10;
+        }
+    }
 
-//function for onclick of all buttons
+    if(getColor == "green"){
+        if(getValue == 1){
+            green++;
+        } else if(getValue == 5){
+            green+= 5;
+        } else if(getValue == 10){
+            green+= 10;
+        }
+    }
 
-//buttons for blue
+    if(getColor == "blue"){
+        if(getValue == 1){
+            blue++;
+        } else if(getValue == 5){
+            blue+= 5;
+        } else if(getValue == 10){
+            blue+= 10;
+        }
+    }
 
-//function for onclick of all buttons
-
+    finalColor = "rgb(" + red +", " + green +", " + blue +")"
+    currentColor.innerHTML = "Current Color: " + finalColor;
+    canvas.style.backgroundColor = finalColor;
+}
